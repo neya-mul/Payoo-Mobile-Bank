04eacd7d0f244a40332bbe5aa6c18309d4079269
neya-mul <neyamulislam946@gmail.com>
2026-02-18 12:21:47 +0600

    button interactivity added

---
+++ login.js
@@ -0,0 +1,12 @@
+document.getElementById('login-button').addEventListener('click', function () {
+
+    let mobileNumber = document.getElementById('mobile-number').value;
+    let pinNumber = document.getElementById('pin').value;
+     if(mobileNumber === '01234567890' && pinNumber === '1234'){
+        alert('login successfull');
+        window.location.assign('/home.html')
+     }
+     else{alert('login failed')
+        return;
+     }
+})
