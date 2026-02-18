58fed61f3e144d223ce98d59a0ddbac67e903e44
neya-mul <neyamulislam946@gmail.com>
2026-02-18 16:34:34 +0600

    cash-out section completed

--- cash-out.js
+++ cash-out.js
@@ -1,3 +1,26 @@
-document.getElementById('cash-out').addEventListener('click', function(){
-    console.log('Neyamul')
+document.getElementById('widthdraw-button').addEventListener('click', function () {
+    let money = document.getElementById('money');
+    let currentAmmount = money.innerText;
+    let agentNumber = document.getElementById('agent-number').value;
+    let amount = document.getElementById('amount').value;
+
+    let pin = document.getElementById('pin').value;
+    let newAmount = Number(currentAmmount) - Number(amount);
+
+
+    if (agentNumber === '01234567890' && pin === '1234') {
+        if (Number(currentAmmount) < Number(amount)) {
+            alert('Unsufficient Amount');
+            return;
+        }
+
+        else {
+            alert('Withdraw successful');
+            money.innerText = newAmount;
+        }
+    }
+    else {
+        alert('Invalid Information');
+    }
+
 })
