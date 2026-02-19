a081aa04730d6d495d4b0ec0572e7280404b77fc
neya-mul <neyamulislam946@gmail.com>
2026-02-19 11:25:32 +0600

    animation added

--- login.js
+++ login.js
@@ -1,12 +1,13 @@
 document.getElementById('login-button').addEventListener('click', function () {
 
-    let mobileNumber = document.getElementById('mobile-number').value;
-    let pinNumber = document.getElementById('pin').value;
-     if(mobileNumber.length === 11 && pinNumber.length === 4){
-        alert('login successfull');
-        window.location.assign('./home.html')
-     }
-     else{alert('login failed')
-        return;
-     }
+   let mobileNumber = document.getElementById('mobile-number').value;
+   let pinNumber = document.getElementById('pin').value;
+   if (mobileNumber.length === 11 && pinNumber.length === 4) {
+      alert('login successfull');
+      window.location.assign('./home.html')
+   }
+   else {
+      alert('login failed')
+      return;
+   }
 })
